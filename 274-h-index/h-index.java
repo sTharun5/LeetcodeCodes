4f7f0562af42c45abb2669e60f8f81bffac5fb0a
Tharun S <119474892+sTharun5@users.noreply.github.com>
2026-06-17 17:33:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (98.87%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,25 +1,19 @@
 class Solution {
     public int hIndex(int[] citations) {
         int freq[] = new int[citations.length+1];
-
-        for(int c : citations){
-            if(c >= citations.length){
-                freq[citations.length]++;
-            }
-            else{
-                freq[c]++;
-            }
+        for(int i =0;i<citations.length;i++){
+            if(citations[i] >= citations.length)
+            freq[citations.length]++;
+            else
+            freq[citations[i]]++;
         }
+
         int cnt = 0;
+
         for(int i=citations.length;i>=0;i--){
             cnt = cnt + freq[i];
-            if(cnt >= i){
-                return i;
-            }
+            if(cnt >= i) return i;
         }
-
         return 0;
-
-
     }
 }
