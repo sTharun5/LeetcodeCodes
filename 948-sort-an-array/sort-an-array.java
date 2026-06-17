333216759a050405954e38a66ae611fa13f5dcab
Tharun S <119474892+sTharun5@users.noreply.github.com>
2026-06-17 07:05:42 +0530

    Time: 2577 ms (5.03%) | Memory: 86 MB (43.82%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,47 +1,17 @@
 class Solution {
-
-    public void merger(int[]nums,int low,int mid,int high){
-        int ls = mid-low+1;int rs = high-mid;
-        int la[] = new int[ls];
-        int ra[] = new int[rs];
-
-        int i=0;int j=0;
-        for(i=0;i<ls;i++){
-            la[i] = nums[i+low];
-        }
-        for(j=0;j<rs;j++){
-            ra[j] = nums[j+mid+1];
-        }
-
-        i=0;j=0;
-        int k=low;
-
-        while(i < ls && j < rs){
-            if(la[i] < ra[j]){
-                nums[k++] = la[i++];
-            }
-            else{
-                nums[k++] = ra[j++];
-            }
-        }
-
-        while(i < ls)nums[k++] = la[i++];
-        while(j < rs)nums[k++] = ra[j++];
-
-    }
-
-
-    public void mergeSort(int[]nums,int low,int high){
-        if(low < high){
-            int mid = (low+high) / 2;
-            mergeSort(nums,low,mid);
-            mergeSort(nums,mid+1,high);
-            merger(nums,low,mid,high);
-        }
-    }
     public int[] sortArray(int[] nums) {
-        
-        mergeSort(nums,0,nums.length-1);
+      
+
+        for(int i = 1; i < nums.length; i++){
+                int key = nums[i];
+                int j = i-1;
+
+                while(j>=0 && nums[j] > key){
+                    nums[j+1] = nums[j];
+                    j--;
+                }
+                nums[j+1] = key;
+        }
         return nums;
     }
 }
