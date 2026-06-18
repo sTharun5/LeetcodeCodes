8bce36971693ab9ed8de025811a30d56cbb12dbd
Tharun S <119474892+sTharun5@users.noreply.github.com>
2026-06-18 13:38:29 +0530

    Time: 8 ms (90.99%) | Memory: 49.1 MB (57.91%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,43 +1,24 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-      
-
-        List<int[]> temp = new ArrayList<>();
-
-        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
         int current[] = intervals[0];
-        int k = 0;
-     
-      
-        for(int i = 1; i<intervals.length;i++){
+        ArrayList<int[]> store = new ArrayList<>();
+
+        for(int i=0;i<intervals.length;i++){
             if(current[1] >= intervals[i][0]){
                 current[1] = Math.max(intervals[i][1],current[1]);
             }
             else{
-           
-               int [] s = new int[2];
-               s[0] = current[0];
-               s[1] = current[1];
-               temp.add(s);
-                current[0] = intervals[i][0];
-                current[1] = intervals[i][1];
-
+                store.add(current);
+                current = intervals[i];
             }
         }
-        int[] s = new int[2];
-        s[0] = current[0];
-        s[1] = current[1];
-        temp.add(s);
-         int [][] ans = new int[temp.size()][2];
-        
-       
-        for(int i=0;i<temp.size();i++){
-            int []val = temp.get(i);
-            ans[i][0] = val[0];
-            ans[i][1] = val[1];
-            
+        store.add(current);
+        int ans[][] = new int[store.size()][2];
+        for(int i=0;i<store.size();i++){
+            ans[i][0] = store.get(i)[0];
+            ans[i][1] = store.get(i)[1];
         }
-
         return ans;
         
     }
