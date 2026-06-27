d51a676462cefdbcf6ec8a8a7101f0dd0fb8b169
Tharun S <119474892+sTharun5@users.noreply.github.com>
2026-06-27 13:31:28 +0530

    Time: 18 ms (81.87%) | Memory: 97.9 MB (82.45%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,18 +1,21 @@
 class Solution {
 
     public void combo(int i,int n,int k, List<List<Integer>> ans,List<Integer> temp){
-        if(k == 0){
+        if(k <= 0){
             ans.add(new ArrayList<>(temp));
             return;
         }
 
         if(i > n)return;
+      
+     
 
-            temp.add(i);
-            combo(i+1,n,k-1,ans,temp);
+        for(int j = i; j <= n ; j++){
+            temp.add(j);
+            combo(j+1,n,k-1,ans,temp);
             temp.remove(temp.size()-1);
-            combo(i+1,n,k,ans,temp);
-        
+           
+        }
     }
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> ans = new ArrayList<>();
